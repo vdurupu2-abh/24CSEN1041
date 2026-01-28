4e4032896664ec24ad7d48d6defe6239db988ed0
vdurupu2-abh <vdurupu2@student.gitam.edu>
2026-01-28 11:22:33 +0530

    precedence-associativity.py

--- precedence-associativity.py
+++ precedence-associativity.py
@@ -1,12 +1,10 @@
-# Precedence and associativity of operators
-
 a = 5
 b = 2
 
 c = a + b * 3
 print(f"Value of c = {c}")
 
-c = a // b * 4   # use // for integer division like C
+c = a // b * 4   
 print(f"Value of c = {c}")
 
 c = 3 % b * a
