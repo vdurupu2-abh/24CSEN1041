3bc2cd85ea7c94dbde43c789f53fcab5052e35dd
vdurupu2-abh <vdurupu2@student.gitam.edu>
2026-01-28 11:38:10 +0530

    operators.py

--- a = 9 b = 5  # Arithmetic Operators print("Arithmetic Operators") print(f"{a} + {b} = {a + b}") print(f"{a} - {b} = {a - b}") print(f"{a} * {b} = {a * b}") print(f"{a} / {b} = {a /operators.py
+++ a = 9 b = 5  # Arithmetic Operators print("Arithmetic Operators") print(f"{a} + {b} = {a + b}") print(f"{a} - {b} = {a - b}") print(f"{a} * {b} = {a * b}") print(f"{a} / {b} = {a /operators.py
@@ -33,3 +33,30 @@ print(f"Right Shift {a} >> 2 = {a >> 2}")
 # Conditional (Ternary) Operator
 print("\n" + ("a is greater than b" if a > b else "b is less than a"))
 
+output
+Arithmetic Operators
+9 + 5 = 14
+9 - 5 = 4
+9 * 5 = 45
+9 / 5 = 1.80
+9 % 5 = 4
+
+Relational Operators
+9 < 5 = False
+9 > 5 = True
+9 == 5 = False
+9 != 5 = True
+
+Logical Operators
+AND 9 and 5 = True
+OR 9 or 5 = True
+NOT 9 = False
+
+Bitwise Operators
+9 & 5 = 1
+9 | 5 = 13
+Bitwise XOR 9 ^ 5 = 12
+Left Shift 9 << 2 = 36
+Right Shift 9 >> 2 = 2
+
+a is greater than b
