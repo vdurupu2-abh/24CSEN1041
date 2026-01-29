5e9706fa951185926320ac023753a914c6cb694c
vdurupu2-abh <vdurupu2@student.gitam.edu>
2026-01-29 14:51:27 +0530

    Create use_of_pass_continue_break.py

---
+++ use_of_pass_continue_break.py
@@ -0,0 +1,24 @@
+
+for i in range(1, 100):
+    if i % 2 == 0:
+        continue  
+    elif i == 25:
+        break     
+        pass      
+    
+    
+    print(i)
+
+Output
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+21
+23
