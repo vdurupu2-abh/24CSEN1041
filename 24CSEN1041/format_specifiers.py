37740a99d7d176d619916142656e2f0ba9640cc5
vdurupu2-abh <vdurupu2@student.gitam.edu>
2026-01-29 14:56:35 +0530

    Create format_specifiers.py

---
+++ 24CSEN1041/format_specifiers.py
@@ -0,0 +1,10 @@
+a = 15
+
+print(f"Decimal representation: {a}")
+print(f"Octal representation: {oct(a)}")
+print(f"Hexadecimal representation: {hex(a)}")
+
+Output
+Decimal representation: 15
+Octal representation: 0o17
+Hexadecimal representation: 0xf
