0f8fcdaab253f75f450c78215bd79ac6e9ed88f2
vdurupu2-abh <vdurupu2@student.gitam.edu>
2026-03-05 14:18:34 +0530

    Create tuple_related.py

---
+++ tuple_related.py
@@ -0,0 +1,17 @@
+tuple_1 = ("2025475663", "2025102233", "2025348284")
+print(tuple_1)
+print(type(tuple_1))
+print(len(tuple_1))
+# Tuples are immutable just like strings
+
+# unpacking of a tuple
+sno_1, sno_2, sno_3 = tuple_1
+print(sno_1, sno_2, sno_3)
+
+OUTPUT
+('2025475663', '2025102233', '2025348284')
+<class 'tuple'>
+3
+2025475663 2025102233 2025348284
+
+=== Code Execution Successful ===
