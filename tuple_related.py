7eafea7741804a1032c15eddef6c582b3da49f95
vdurupu2-abh <vdurupu2@student.gitam.edu>
2026-03-05 14:23:56 +0530

    tuple_related.py

--- tuple_related.py
+++ tuple_related.py
@@ -2,9 +2,7 @@ tuple_1 = ("2025475663", "2025102233", "2025348284")
 print(tuple_1)
 print(type(tuple_1))
 print(len(tuple_1))
-# Tuples are immutable just like strings
 
-# unpacking of a tuple
 sno_1, sno_2, sno_3 = tuple_1
 print(sno_1, sno_2, sno_3)
 
